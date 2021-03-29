044f2efb7373e7a3255a77ec7f30bc050b541f65
Jenny Pan <jennypanliu@gmail.com>
2021-03-29 17:33:07 -0500

    Added grid, event listeners, and buttons

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,28 @@
+const grid = document.querySelector('.grid');
+const clearButton = document.createElement('button');
+
+let gridSize = 16*16-2;
+
+
+for(i = -1; i <= gridSize; i++){
+    const gridElement = document.createElement('div');
+    gridElement.classList.add(`${i}`, `gridElement`);
+    grid.appendChild(gridElement);
+}
+gridColor = document.querySelectorAll('.gridElement');
+
+gridColor.forEach(applyColor =>
+    applyColor.addEventListener('mouseover', () => {
+    applyColor.style.background = ('green');
+     })
+    );
+
+clearButton.style.width = ('50px');
+clearButton.innerText = ('clear');
+grid.appendChild(clearButton);
+
+clearButton.onclick = function() {
+    gridColor.forEach(clearGrid => {
+        clearGrid.style.background = ('white');
+    });
+};
